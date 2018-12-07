1226bd66dd035e38b30e836b5b54bd93e8489881
Thom Staal <thom_staal@hotmail.com>
2018-12-07 17:40:50 +0100

    erwgerg

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ io.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
     socket.emit('test', 'Disconnected...');
-  })
+  });
 
   socket.on('message', function (message) {
     io.emit('message', message);
-  })
+  });
 
   socket.on('notice', function(message){
     io.emit('notice', message);
-  })
+  });
 
 });
 
 
 http.listen(3000, function () {
-  console.log('listening on *:3000')
+  console.log('listening on *:3000');
 });
