7e089b894803f8095306f6b1b77802f341907c90
Thom Staal <thom_staal@hotmail.com>
2018-12-07 17:33:50 +0100

    yuk

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const aws = require('aws-sdk');
+var PORT = process.env.PORT || 5000;
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
